058923bf67bfa40433c3519143a0c3cb32fb34bf
Aniwat99 <rst14j6@gmail.com>
2025-06-19 02:42:25 +0000

    63

--- cell06/ex03/greetings_for_all.py
+++ cell06/ex03/greetings_for_all.py
@@ -1,19 +1,10 @@
-import sys
+def greetings(name="noble stranger"):
+    if isinstance(name, str):
+        print(f"Hello, {name}.")
+    else:
+        print("Error! It was not a name.")
 
-def shrink(i):
-    print(i[:8])
-
-def enlarge(i):
-    print(i + 'z' * (8 - len(i)))
-
-if len(sys.argv) < 2:
-    print("none")
-else:
-    for arg in sys.argv[1:]:
-        if len(arg) > 8:
-            shrink(arg)
-        elif len(arg) < 8:
-            enlarge(arg)
-        # elif
-        #     print(asg)
-             
+greetings('Trent')
+greetings('Aiexander')
+greetings()
+greetings(23)
